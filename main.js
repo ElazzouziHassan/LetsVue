434649991a6ed898fe0390c73e3b28c06f78250e
wizardy <wizardhassan99@gmail.com>
2023-10-15 20:04:59 +0100

    Mount : updates

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const main = Vue.createApp({
-  template: `
-    <h1 align="center"> Let's VUE !</h1>
-    <p align="center">Hello, World, Lets learn vue js</p>
-  `,
+  // ! - uncomment this to preview how it work
+  // template: `
+  //   <h1 align="center"> Let's VUE !</h1>
+  //   <p align="center">Hello, World, Lets learn vue js</p>
+  // `,
 })
 
 main.mount('.app')
