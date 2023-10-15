67e4dfbfea5c76171e9768226ef1ae8a1f1f397c
wizardy <wizardhassan99@gmail.com>
2023-10-15 20:03:34 +0100

    Mount : adding a template to the app

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Let's VUE !")
+const main = Vue.createApp({
+  template: `
+    <h1 align="center"> Let's VUE !</h1>
+    <p align="center">Hello, World, Lets learn vue js</p>
+  `,
+})
+
+main.mount('.app')
